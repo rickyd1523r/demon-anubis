be8c8a20aa86c6a19a41a7d95e2aa4e7f15bc39b
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-05 16:04:46 +0530

    Create alive.py

---
+++ Exon/modules/alive.py
@@ -0,0 +1,99 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+BSD 2-Clause License
+
+Copyright (C) 2022, Kakashi [https://t.me/Yeahoffline]  
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from Exon import ALIVE_MEDIA, UPDATE_CHANNEL, SUPPORT_CHAT, OWNER_USERNAME, dispatcher, NETWORK, NETWORK_USERNAME
+from Exon.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext
+
+bot_name = f"{dispatcher.bot.first_name}"
+
+ALIVE_ID = ALIVE_MEDIA.split(".")
+alive_id = ALIVE_ID[-1]
+
+def awake(update: Update, context: CallbackContext):
+    message = update.effective_message
+    buttons = [
+        [
+        InlineKeyboardButton(
+            text="Updates",
+            url=f"https://t.me/{UPDATE_CHANNEL}"),
+        InlineKeyboardButton(
+            text="Support",
+            url=f"https://t.me/{SUPPORT_CHAT}"),
+        ],
+     ]
+    
+    first_name = update.effective_user.first_name
+    user = message.from_user
+
+    TEXT = f"""
+    <b>Hey Bit*h <a href="tg://user?id={user.id}">{first_name}</a> ❤️, I'm {bot_name} Robot. I'm alive and working fine!!! 
+
+ 💠 My Owner : <a href="https://t.me/{OWNER_USERNAME}">{OWNER_USERNAME}</a>
+ 💠 Telethon Version: 1.26.0
+ 💠 Pyrogram Version: 2.0.70
+ 💠 Python Version: 3.10 </b>
+
+    """
+    if NETWORK:
+        TEXT = TEXT + f'\n⚪ <b>Join my support group : <a href="https://t.me/{NETWORK_USERNAME}">{NETWORK}</a>\n\n' + 'Thanks For Adding Me Here ❤️</b>'
+    
+    else:
+        TEXT = TEXT + "\n<b>Thanks For Adding Me Here ❤️</b>"
+
+    try:
+        if alive_id in ("jpeg", "jpg", "png"):
+            message.reply_photo(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        elif alive_id in ("mp4", "mkv"):
+            message.reply_video(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        elif alive_id in ("gif", "webp"):
+            message.reply_animation(ALIVE_MEDIA, caption=TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+        else:
+            message.reply_text(TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+
+    except:
+        message.reply_text(TEXT, reply_markup=InlineKeyboardMarkup(buttons),parse_mode=ParseMode.HTML)
+
+ALIVE_HANDLER = DisableAbleCommandHandler("alive", awake, run_async=True)
+dispatcher.add_handler(ALIVE_HANDLER)
+__command_list__ = ["alive"]
+__handlers__ = [
+    ALIVE_HANDLER,
+]
+
+__mod_name__ = "Alive ✨"
+__help__ = """
+*ALIVE*
+ ❍ `/alive` :Check BOT status
+"""
