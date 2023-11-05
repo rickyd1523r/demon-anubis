b605058ff7993c5825c459fba1edc1e55acf9f88
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-05 19:53:38 +0530

    Add files via upload

---
+++ Exon/utils/captcha_mongo.py
@@ -0,0 +1,59 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+BSD 2-Clause License
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from pymongo import MongoClient
+
+from Shikimori import MONGO_DB_URI
+
+
+class manage_db:
+    def __init__(self):
+        self.db = MongoClient(MONGO_DB_URI)["captcha"]
+        self.chats = self.db["Chats"]
+
+    def chat_in_db(self, chat_id):
+        chat = self.chats.find_one({"chat_id": chat_id})
+        return chat
+
+    def add_chat(self, chat_id, captcha):
+        if self.chat_in_db(chat_id):
+            return 404
+        self.chats.insert_one({"chat_id": chat_id, "captcha": captcha})
+        return 200
+
+    def delete_chat(self, chat_id):
+        self.chats.delete_many({"chat_id": chat_id})
+        return True
