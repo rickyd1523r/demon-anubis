16a30d69365317646fbbbb98e09e652fa6142ba8
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-05 20:32:00 +0530

    Update events.py

--- Exon/events.py
+++ Exon/events.py
@@ -1,61 +1,13 @@
-"""
-STATUS: Code is working. ✅
-"""
-
-"""
-BSD 2-Clause License
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-Credits:-
-    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
-import inspect
-import logging
-import sys
-import re
-
-from pathlib import Path
 from telethon import events
 
-from pymongo import MongoClient
-from Exon import MONGO_DB_URI
 from Exon import telethn
 
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["Shikimorirobot"]
-gbanned = db.gban
 
 def register(**args):
-    """ Registers a new message. """
-    pattern = args.get("pattern", None)
+    """ʀᴇɢɪsᴛᴇʀs ᴀ ɴᴇᴡ ᴍᴇssᴀɢᴇ."""
+    pattern = args.get("pattern")
 
-    r_pattern = r"^[/!.]"
+    r_pattern = r"^[/!]"
 
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
@@ -69,144 +21,9 @@ def register(**args):
     return decorator
 
 
-def chataction(**args):
-    """ Registers chat actions. """
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.ChatAction(**args))
-        return func
-
-    return decorator
-
-
-def userupdate(**args):
-    """ Registers user updates. """
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.UserUpdate(**args))
-        return func
-
-    return decorator
-
-
-def inlinequery(**args):
-    """ Registers inline query. """
-    pattern = args.get("pattern", None)
-
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.InlineQuery(**args))
-        return func
-
-    return decorator
-
-
-def callbackquery(**args):
-    """ Registers inline query. """
-
+def Asuinline(**args):
     def decorator(func):
         telethn.add_event_handler(func, events.CallbackQuery(**args))
         return func
 
     return decorator
-
-
-def bot(**args):
-    pattern = args.get("pattern")
-    r_pattern = r"^[/]"
-
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    args["pattern"] = pattern.replace("^/", r_pattern, 1)
-    stack = inspect.stack()
-    previous_stack_frame = stack[1]
-    file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
-    reg = re.compile("(.*)")
-
-    if pattern is not None:
-        try:
-            cmd = re.search(reg, pattern)
-            try:
-                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
-            except BaseException:
-                pass
-
-            try:
-                FUN_LIST[file_test].append(cmd)
-            except BaseException:
-                FUN_LIST.update({file_test: [cmd]})
-        except BaseException:
-            pass
-
-    def decorator(func):
-        async def wrapper(check):
-            if check.edit_date:
-                return
-            if check.fwd_from:
-                return
-            if check.is_group or check.is_private:
-                pass
-            else:
-                print("i don't work in channels")
-                return
-            if check.is_group:
-               if check.chat.megagroup:
-                  pass
-               else:
-                  print("i don't work in small chats")
-                  return
-                          
-            users = gbanned.find({})
-            for c in users:
-                if check.sender_id == c["user"]:
-                    return
-            try:
-                await func(check)
-                try:
-                    LOAD_PLUG[file_test].append(func)
-                except Exception:
-                    LOAD_PLUG.update({file_test: [func]})
-            except BaseException:
-                return
-            else:
-                pass
-
-        telethn.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
-
-    return decorator
-
-
-def EmikoRobot(**args):
-    pattern = args.get("pattern", None)
-    disable_edited = args.get("disable_edited", False)
-    ignore_unsafe = args.get("ignore_unsafe", False)
-    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
-    group_only = args.get("group_only", False)
-    disable_errors = args.get("disable_errors", False)
-    insecure = args.get("insecure", False)
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    if "disable_edited" in args:
-        del args["disable_edited"]
-
-    if "ignore_unsafe" in args:
-        del args["ignore_unsafe"]
-
-    if "group_only" in args:
-        del args["group_only"]
-
-    if "disable_errors" in args:
-        del args["disable_errors"]
-
-    if "insecure" in args:
-        del args["insecure"]
-
-    if pattern:
-        if not ignore_unsafe:
-            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
