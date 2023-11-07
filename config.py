5c55a9707f06b5cdbef3a396aa5d9d8e6b4266e8
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-07 22:04:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ class Config(object):
 
     API_ID = int(getenv("API_ID", 6))
     API_HASH = getenv("API_HASH", None)
-    ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
+    ARQ_API_KEY = "HDEBTP-URNMLY-BUQXRQ-RRSUPF-ARQ"
     STRING_SESSION = getenv("STRING_SESSION", None)
     SPAMWATCH_API = None
     TOKEN = getenv("TOKEN", None)
     OWNER_ID = int(getenv("OWNER_ID", "6495264484"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
     OWNER_USERNAME = getenv("OWNER_USERNAME", "YeahKAKASHl")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "no_rolling")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001712227681"))
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "rose_orianalogs")
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002143522204"))
     MONGO_URI = getenv("MONGO_DB_URI")
     REDIS_URL = "redis://default:SeeHVAqRRbzHNEQ1gFac5rZIui5whbpQ@redis-17680.c289.us-west-1-2.ec2.cloud.redislabs.com:17680"
     DATABASE_URL = getenv("DATABASE_URL")
