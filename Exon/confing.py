582c6058adcc20bf45369a654058f45169d5ea70
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-07 16:43:55 +0530

    Add files via upload

---
+++ Exon/confing.py
@@ -0,0 +1,129 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+BSD 2-Clause License
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import os
+import sys
+
+import yaml
+from envparse import env
+
+from Shikimori.utils.logger import log
+
+DEFAULTS = {
+    "LOAD_MODULES": True,
+    "DEBUG_MODE": True,
+    "REDIS_HOST": "localhost",
+    "REDIS_PORT": 6379,
+    "REDIS_DB_FSM": 1,
+    "MONGODB_URI": "localhost",
+    "MONGO_DB": "Shikimori",
+    "API_PORT": 8080,
+    "JOIN_CONFIRM_DURATION": "30m",
+}
+
+CONFIG_PATH = "data/bot_conf.yaml"
+if os.name == "nt":
+    log.debug("Detected Windows, changing config path...")
+    CONFIG_PATH = os.getcwd() + "\\data\\bot_conf.yaml"
+
+if os.path.isfile(CONFIG_PATH):
+    log.info(CONFIG_PATH)
+    for item in (
+        data := yaml.load(open("data/bot_conf.yaml", "r"), Loader=yaml.CLoader)
+    ) :
+        DEFAULTS[item.upper()] = data[item]
+else:
+    log.info("Using env vars")
+
+
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_int_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.int(name, default=default)) and not required:
+        log.warn("No int key: " + name)
+        return None
+    elif not data:
+        log.critical("No int key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_list_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.list(name, default=default)) and not required:
+        log.warn("No list key: " + name)
+        return []
+    elif not data:
+        log.critical("No list key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_bool_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.bool(name, default=default)) and not required:
+        log.warn("No bool key: " + name)
+        return False
+    elif not data:
+        log.critical("No bool key: " + name)
+        sys.exit(2)
+    else:
+        return data
