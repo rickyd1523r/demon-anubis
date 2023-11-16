787d96149c520ce49f1a3dd2b6fb416be4faa8a0
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-16 20:51:44 +0530

    Update stickers.py

--- Exon/modules/stickers.py
+++ Exon/modules/stickers.py
@@ -614,7 +614,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./Shikimori/resources/default.ttf"
+        fnt = "./Exon/resources/default.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
