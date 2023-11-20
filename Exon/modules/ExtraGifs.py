dd2652c89a99e2a64742fd3dd67f4247bfc86862
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-11-20 21:11:11 +0530

    Update ExtraGifs.py

--- Exon/modules/ExtraGifs.py
+++ Exon/modules/ExtraGifs.py
@@ -141,7 +141,7 @@ def pat(update: Update, context: CallbackContext):
         user1 = bot.first_name
         user2 = curr_user
 
-    pat_type = random.choice(("Text", "Gif", "Sticker"))
+    pat_type = random.choice(("Gif", "Sticker"))
     if pat_type == "Gif":
         try:
             temp = random.choice(ExtraGifs_strings.PAT_GIFS)
@@ -156,7 +156,7 @@ def pat(update: Update, context: CallbackContext):
         except BadRequest:
             pat_type = "Gif"
 
-    if pat_type == "Text":
+    if pat_type == "Gif":
         temp = random.choice(ExtraGifs_strings.PAT_TEMPLATES)
         reply = temp.format(user1=user1, user2=user2)
         reply_to.reply_text(reply, parse_mode=ParseMode.HTML)
